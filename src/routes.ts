2e6344d37ea0fc28cdbe683ced972e5dcb345515
rasoviti <rrasoviti@gmail.com>
2021-06-30 21:41:55 -0300

    corrigindo rota

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,10 @@
 import { Router, Request, Response } from "express";
-import { CreateCandlestickController } from "./controllers/CreateCandlestickController";
 import { ShowCandlestickController } from "./controllers/ShowCandlesticksController";
 
 const router = Router();
 
-const createCandlestickController = new CreateCandlestickController();
 const showCandlestickController = new ShowCandlestickController();
 
-router.post("/candlesticks", createCandlestickController.handle);
-router.get("/", showCandlestickController.handle);
+router.get("/candlesticks", showCandlestickController.handle);
 
 export { router }
